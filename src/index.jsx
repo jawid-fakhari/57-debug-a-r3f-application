14e5e9e3d5ff99d6590306151fa25edbbc93f4a3
jawid-fakhari <jawid_fakhari@live.it>
2024-10-10 11:33:46 +0200

    Configure Leva

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import { StrictMode } from 'react'
+import { Leva } from 'leva'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <StrictMode>
+        {/* per configurare Leva lo dobbiamo aggiungere fuori canvas */}
+        <Leva collapsed />{/* theme={theme}, fill, flat, onLineLabels, hideTitleBar, collapsed, hidden */}
         <Canvas
             camera={{
                 fov: 45,
